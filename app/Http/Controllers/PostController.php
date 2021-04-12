01bc27ad59757d8069f02924f71bde3564778490
204s415s <204s415s@stu.kobe-u.ac.jp>
2021-04-12 18:24:28 +0900

    overwrite

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers; //PostController.phpはここにあるよ！
+
+use App\Post; //作成したPostモデルクラスを使うよ！
+use App\Http\Requests\PostRequest; //作成したリクエストクラスを使うよ！
+use Illuminate\Http\Request;
+
+class PostController extends Controller {
+	/**
+	* Post一覧を表示する
+	* 
+	* @param：引数 Post Postモデル
+	* @return array Postモデルリスト
+	* Postテーブルの配列を返す、といった感じ　なのか？
+	*/
+	//記事一覧画面
+	public function index(Post $post) {
+	    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+	}
+	
+	/**
+	* 特定IDのpostを表示する
+	*
+	* @params：引数 Object Post // 引数の$postはid=1のPostインスタンス
+	* @return Reposnse post view
+	*/
+
+	//記事詳細画面
+	public function show(Post $post) {
+    	return view('show')->with(['post' => $post]);
+	}
+	
+	//記事作成画面
+	public function create() {
+		return view('create');
+	}
+	
+	//入力されたデータをDBに送り、詳細画面にリダイレクトをする
+	public function store(Post $post, PostRequest $request)	{
+    	$input = $request['post'];
+	    $post->fill($input)->save();
+    	return redirect('/posts/' . $post->id);
+	}
+	
+	//記事の編集画面を出すよ
+	public function edit(Post $post){
+		return view('edit')->with(['post' => $post]);
+	}
+	
+	//記事の編集をするよ
+	public function update(Post $post, PostRequest $request) {
+		$edit = $request['post'];
+		$post->fill($edit)->save();
+		return redirect('/posts/' . $post->id);
+	}
+}
+	
+?>
+	
---
+++ resources/views/edit.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Blog</title>
+    </head>
+    
+    <body>
+        <h1>記事の編集</h1>
+            <form action="/posts/{{ $post->id }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="content_title">
+                    <h2>Title</h2>
+                    <input type="text" name="post[title]" value="{{ $post->title }}"/>
+                    <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+                </div>
+                <div class="content_body">
+                    <h2>Body</h2>
+                    <textarea name="post[body]" value="{{ old('post.title')}}">{{ $post->body }}</textarea>
+                    <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
+                </div>    
+                <input type="submit" value="保存" />
+            </form>
+            <div class="back">[ <a href="/">back</a> ]</div>
+    </body>
+</html>
+        
