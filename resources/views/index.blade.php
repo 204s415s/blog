cdd17d75f0298aeefec1780164bcccd3e4cc152c
204s415s <204s415s@stu.kobe-u.ac.jp>
2021-04-15 15:05:53 +0900

    08-06

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
     
     <body>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,12 +10,26 @@
     
     <body>
         <h1>Blog Name</h1>
+            <div class="edit">[ <a href="/posts/{{ $post->id }}/edit">edit</a> ]</div>
+            <form action="/posts/{{$post->id}}" id="form_delete" method="post" style="display:inline">
+                @csrf
+                @method('DELETE')
+                <input type="submit" style="display:none" />
+                <p class="delete">[ <span onclick="return deletePost(this);">delete</span> ]</p>
+            </form>
             <div class="post">
                 <h2 class="title">{{ $post->title }}</h2>
                 <p class="body">{{ $post->body }}</p>
                 <p class="updated_at">{{ $post->updated_at }}</p>
             </div>
-            <div class="edit">[ <a href="/posts/{{ $post->id }}/edit">edit</a> ]</div>
             <div class="back">[ <a href="/">back</a> ]</div>
+            <script>
+                function deletePost(e) {
+                    'use strict';
+                    if (confirm('本当に削除しますか？')) {
+                        document.getElementById('form_delete').submit();
+                    }
+                }
+            </script>
     </body>
         
